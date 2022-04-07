c05dddfc722d56a4fd5541ce53655f9e2bb46778
misguna4646 <misganategegn868@gmail.com>
2022-04-07 09:22:30 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * my hello, printf task on alx
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

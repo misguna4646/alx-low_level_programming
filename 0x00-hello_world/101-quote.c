fd5b1ea63e37a236b36f025f47c9bd17b018f53b
misguna4646 <misganategegn868@gmail.com>
2022-04-08 04:52:53 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
  *main - entry point
  *
  *Return: 1 after printing the function
+ *
+ * alx project task8
+ * written by misgana
  */
 int main(void)
 {
-	FILE *fp;
-fp = fopen("file.txt", "w+");
-fputs("and that piece of art is useful\"", fp);
-fputs(" - Dora Korpar, 2015-10-19", fp);
-fclose(fp);
-return (1);
-}
+	write(2,
+		"and that piece of art is useful\" - Dora, 2015-10-19\n", 59);
+	return (1);

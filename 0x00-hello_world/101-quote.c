3d7a2fd03fef714062b7093acf7b94258f4ab22e
misguna4646 <misganategegn868@gmail.com>
2022-04-08 03:50:53 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	FILE *fp;
+
+   fp = fopen("file.txt", "w+");
+fputs("and that piece of art is useful\"", fp);
+		fputs("- Dora Korpar, 2015-10-19", fp);
+		fclose(fp);
 return (1);
 }

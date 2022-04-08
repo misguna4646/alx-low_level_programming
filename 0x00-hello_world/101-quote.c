061ee3a9d551a7724325079a5db497847d46ec46
misguna4646 <misganategegn868@gmail.com>
2022-04-08 03:58:12 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 
    fp = fopen("file.txt", "w+");
 fputs("and that piece of art is useful\"", fp);
-		fputs("- Dora Korpar, 2015-10-19", fp);
-		fclose(fp);
+fputs(" - Dora Korpar, 2015-10-19", fp);
+fclose(fp);
 return (1);
 }
